80a9b5d68a895bd6853edc27306a67ed6d86264c
Edi Paulino da Silva <edipaulinodasilva@gmail.com>
2021-06-10 23:09:41 -0300

    adequando padrões

--- app/utils/UrlParser.php
+++ app/utils/UrlParser.php
@@ -1,8 +1,23 @@
 <?php
-class UrlParser {
+class UrlParser
+{
+    public function __construct()
+    {        
+    }
+
     //Por base das barras da url será dividido em um array
-    public function parserUrl () {
-        return explode("/", rtrim($_GET['url']), FILTER_SANITIZE_URL);
+    public function parserUrl ()
+    {
+        return explode("/", $this->validateUrl(), FILTER_SANITIZE_URL);
+    }
+
+    private function validateUrl()
+    {
+        $get = $_GET['url'];
+        if (substr($get, -1) == "/") {
+            $get = substr($get, 0, strlen($get) - 1);
+        }
+        return trim($get);
     }
 }
 ?>
