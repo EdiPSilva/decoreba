5dedf9ae77cd199f77e76b22d2da60e55896c203
Edi Paulino da Silva <edipaulinodasilva@gmail.com>
2021-06-10 23:08:17 -0300

    adequando padrões

--- app/ManagementController.php
+++ app/ManagementController.php
@@ -2,37 +2,40 @@
 require_once('utils/Routes.php');
 
 //require de todos os arquivos do diretório controllers
-$dir = __DIR__ . DIRECTORY_SEPARATOR.'controllers';
-$files = glob($dir . DIRECTORY_SEPARATOR . '*.php');
+$dir = __DIR__ . DIRECTORY_SEPARATOR . DIRECTORYCONTROLLERS . '*.php';
+$files = glob($dir);
 foreach ($files as $file) {
     require_once($file);
 }
 
-class ManagementController extends Routes {
-
+class ManagementController extends Routes
+{
     private $methodo;
     private $parameters = [];
     private $object;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();//Chama o construto da classe em extends
         $this->addController();
     }
 
     //Método de adição de controller
-    private function addController() {
+    private function addController()
+    {
         $controllerRoute = $this->getControllerByRote();
-        //$controllerResult = DIRECTORYCONTROLLERS . $controllerRoute ;
         $this->object = new $controllerRoute;
     }
 
     //Método de adição de método de controller
-    private function addMethod() {
+    private function addMethod()
+    {
 
     }
 
     //Método de adição de parâmetros do controller
-    private function addParameter() {
+    private function addParameter()
+    {
 
     }
 }
