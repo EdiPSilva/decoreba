3b5c25e36a02ef5680cd5d232829082295c40d69
Edi Paulino da Silva <edipaulinodasilva@gmail.com>
2021-06-13 21:20:53 -0300

    add link style

--- app/views/templates/Head.php
+++ app/views/templates/Head.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Édi P Silva">
-    <meta name="description" content="Site para estudo de inglês">
+    <meta name="author" content="<?= AUTHOR ?>">
+    <meta name="description" content="<?= DESCRIPTION ?>">
     <meta name="keywords" content="">
     <title><?= $this->getTile(); ?></title>
+
+    <link rel="stylesheet" type="text/css" href="<?= LINKCSS?>">
 </head>
